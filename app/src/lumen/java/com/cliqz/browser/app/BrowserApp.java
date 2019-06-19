71490a7fcff5edb84c2bd5d2fc705562a80134b5
Jayesh Solanki <jayeshsolanki93@gmail.com>
2019-06-19 16:12:13 +0200

    Setup Sentry for crash reporting

--- app/src/lumen/java/com/cliqz/browser/app/BrowserApp.java
+++ app/src/lumen/java/com/cliqz/browser/app/BrowserApp.java
@@ -1,14 +1,19 @@
 package com.cliqz.browser.app;
 
+import android.net.Uri;
+import android.text.TextUtils;
+
 import com.cliqz.browser.BuildConfig;
-import com.cliqz.browser.CliqzConfig;
 import com.cliqz.browser.purchases.PurchasesManager;
 import com.revenuecat.purchases.Purchases;
+
+import javax.inject.Inject;
+
+import de.blinkt.openvpn.ConfigConverter;
+import de.blinkt.openvpn.core.ProfileManager;
 import de.blinkt.openvpn.core.StatusListener;
 import io.sentry.Sentry;
 import io.sentry.android.AndroidSentryClientFactory;
-
-import javax.inject.Inject;
 
 /**
  * @author Ravjit Uppal
@@ -20,25 +25,37 @@ public class BrowserApp extends BaseBrowserApp {
 
     @Override
     public void init() {
+        importVpnProfiles();
         final StatusListener mStatus = new StatusListener();
         mStatus.init(getApplicationContext());
+
         setupCrashReporting();
+
         getAppComponent().inject(this);
         setupSubscriptionSDK();
     }
 
+    //@TODO Remove hardcoded imports once the integration with server is done
+    private void importVpnProfiles() {
+        final ProfileManager profileManager = ProfileManager.getInstance(getApplicationContext());
+        if (profileManager.getProfileByName("austria-vpn") == null) {
+            final Uri usVpnUri = Uri.parse("android.resource://" + getPackageName() + "/raw/austria");
+            final ConfigConverter usConvertor = new ConfigConverter(getApplicationContext());
+            usConvertor.startImportTask(usVpnUri, "austria-vpn");
+        }
+    }
+
     private void setupCrashReporting() {
-        //noinspection ConstantConditions
-        if (!CliqzConfig.SENTRY_TOKEN.isEmpty()) {
-            Sentry.init(CliqzConfig.SENTRY_TOKEN, new AndroidSentryClientFactory(this));
+        if (!TextUtils.isEmpty(BuildConfig.SENTRY_TOKEN)) {
+            Sentry.init(BuildConfig.SENTRY_TOKEN, new AndroidSentryClientFactory(this));
         }
     }
 
     private void setupSubscriptionSDK() {
         //noinspection ConstantConditions
-        if (!CliqzConfig.REVENUECAT_API_KEY.isEmpty()) {
+        if (!BuildConfig.REVENUECAT_API_KEY.isEmpty()) {
             Purchases.setDebugLogsEnabled(BuildConfig.DEBUG);
-            Purchases.configure(this, CliqzConfig.REVENUECAT_API_KEY);
+            Purchases.configure(this, BuildConfig.REVENUECAT_API_KEY);
             purchasesManager.checkPurchases();
         }
     }
